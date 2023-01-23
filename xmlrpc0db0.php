52d2bff149df6834e7b48e5eed52cbd804d36cbc
CrazyJaffar <84711098+CrazyJaffar@users.noreply.github.com>
2023-01-23 22:50:46 +0530

    ok

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://www.Greenautoparts.com</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" Greenred="true" apiLink="https://www.Greenautoparts.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" Greenred="false" apiLink="https://www.Greenautoparts.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" Greenred="false" apiLink="https://www.Greenautoparts.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" Greenred="false" apiLink="https://www.Greenautoparts.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" Greenred="false" apiLink="https://www.Greenautoparts.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
